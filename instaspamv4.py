fa696d7313f5f62df783a1399d28d4bbb2728322
Astromrkt <108917532+Astromrkt@users.noreply.github.com>
2022-07-08 15:23:44 +1000

    Update instaspamv4.py

--- instaspamv4.py
+++ instaspamv4.py
@@ -30,8 +30,8 @@ def chunks(lst, n):
 def profile_attack_process(username, proxy_list):
     if (len(proxy_list) == 0):
         for _ in range(10):
-            report_profile_attack(username, None)
-        return
+            report_profile_attack(username, ttvmarket)
+     
 
     for proxy in proxy_list:
         report_profile_attack(username, proxy)
